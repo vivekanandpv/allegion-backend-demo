1d6445c65a5529676d71cf9f599b0e873937db5a
vivekanandpv <vivekanandpv@outlook.com>
2021-10-27 18:12:50 +0530

    pg migration files

---
+++ CCAP.Api/Migrations/20211027113533_AddedToUserDomain.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CCAP.Api.Migrations
+{
+    public partial class AddedToUserDomain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsForcedToResetPassword",
+                table: "AppUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLocked",
+                table: "AppUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NWrongAttempts",
+                table: "AppUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsForcedToResetPassword",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsLocked",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "NWrongAttempts",
+                table: "AppUsers");
+        }
+    }
+}
